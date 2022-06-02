4f02e0e9593a7b8710446dbd12ef2e8e8ee7948b
P-Gayle <103328828+P-Gayle@users.noreply.github.com>
2022-06-02 18:28:53 +0100

    Final

--- scripts.js
+++ scripts.js
@@ -8,18 +8,17 @@ const errorBorder = document.querySelector("#email");
 const submitEmail = document.getElementById("form");
 
     if (email.match(pattern)){
-        // form.classList.add("valid");
         form.classList.remove("invalid"); 
-        // submitEmail.submit();
+
     } else {
-        // form.classList.remove("valid");
+      
         form.classList.add("invalid");
         text.innerHTML = "Please provide a valid email";
         text.style.color = "#ff0000";
         errorBorder.style.border = "3px solid hsl(0, 93%, 68%)";   
     }
     if (email == "") {
-        // form.classList.remove("valid");
+      
         form.classList.add("invalid");
     }
 }
