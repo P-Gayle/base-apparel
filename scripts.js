83ef159a344441c6777c913bcda48d71141ff15f
P-Gayle <103328828+P-Gayle@users.noreply.github.com>
2022-06-02 18:21:23 +0100

    Fixed invalid status

--- scripts.js
+++ scripts.js
@@ -1,24 +1,25 @@
+function validation (){
+
 const form = document.querySelector(".form");
 const email = document.querySelector("#email").value;
 const text = document.querySelector("#text");
 const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 const errorBorder = document.querySelector("#email");
-
-function validation (){
+const submitEmail = document.getElementById("form");
 
     if (email.match(pattern)){
-        form.classList.add("valid");
-        form.classList.remove("invalid");
+        // form.classList.add("valid");
+        form.classList.remove("invalid"); 
+        // submitEmail.submit();
     } else {
-        form.classList.remove("valid");
+        // form.classList.remove("valid");
         form.classList.add("invalid");
         text.innerHTML = "Please provide a valid email";
         text.style.color = "#ff0000";
         errorBorder.style.border = "3px solid hsl(0, 93%, 68%)";   
     }
     if (email == "") {
-        form.classList.remove("valid");
-        form.classList.remove("invalid");
-        text.innerHTML = "";
+        // form.classList.remove("valid");
+        form.classList.add("invalid");
     }
 }
